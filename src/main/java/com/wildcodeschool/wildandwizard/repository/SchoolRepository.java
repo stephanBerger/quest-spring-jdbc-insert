5f32cec99e630bba46108f3e9bc12beb5197885b
sBerger <stephone13@gmail.com>
2019-12-08 19:34:20 +0100

    challenge ok

--- src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
+++ src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
@@ -1,16 +1,49 @@
 package com.wildcodeschool.wildandwizard.repository;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import com.wildcodeschool.wildandwizard.entity.School;
 
 public class SchoolRepository {
 
-    private final static String DB_URL = "jdbc:mysql://localhost:3306/spring_jdbc_quest?serverTimezone=GMT";
-    private final static String DB_USER = "h4rryp0tt3r";
-    private final static String DB_PASSWORD = "Horcrux4life!";
+	private final static String DB_URL      = "jdbc:mysql://localhost:3306/spring_jdbc_quest?serverTimezone=GMT";
+	private final static String DB_USER     = "h4rryp0tt3r";
+	private final static String DB_PASSWORD = "Horcrux4life!";
 
-    public School save(String name, Long capacity, String country) {
+	public School save(String name, Long capacity, String country) {
 
-        // TODO : insert a new school in database
-        return null;
-    }
+		// TODO : insert a new school in database
+		try {
+			Connection connection = DriverManager.getConnection(
+					SchoolRepository.DB_URL, SchoolRepository.DB_USER,
+					SchoolRepository.DB_PASSWORD);
+			PreparedStatement statement = connection.prepareStatement(
+					"INSERT INTO school (name, capacity, country) VALUES (?, ?, ?)",
+					Statement.RETURN_GENERATED_KEYS);
+			statement.setString(1, name);
+			statement.setLong(2, capacity);
+			statement.setString(3, country);
+
+			if (statement.executeUpdate() != 1) {
+				throw new SQLException("failed to insert data");
+			}
+
+			ResultSet generatedKeys = statement.getGeneratedKeys();
+
+			if (generatedKeys.next()) {
+				Long id = generatedKeys.getLong(1);
+				return new School(id, name, capacity, country);
+			} else {
+				throw new SQLException("failed to get inserted id");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
